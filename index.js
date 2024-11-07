cbf9086b5971a8eca681f2111e48ed32eea2510c
Thierry <noumbuht@gmail.com>
2024-11-07 19:26:56 +0100

    Add spanEl to make the mouse move from top and left

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ bodyEl.addEventListener("mousemove", (event)=>{
     const xPos = event.offsetX
     const yPos = event.offsetY
     const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
     bodyEl.appendChild(spanEl);
 });
